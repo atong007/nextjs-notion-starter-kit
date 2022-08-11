ccdc6dba613f7873926d7fc48ab3ab33b7fc751f
Charlie007 <hong3801505@gmail.com>
2022-08-11 17:13:10 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e58c52b30c314c24b5f75addbf579f81',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Charlie Dev Blog',
+  domain: 'https://dapper-stop-dfe.notion.site/e58c52b30c314c24b5f75addbf579f81',
+  author: 'Charlie',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Personal Blog About Tech',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
